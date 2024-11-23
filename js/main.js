cec4a0218857e2bf2318afd950071794ca393391
MauricioF68 <mauricioterrones98@gmail.com>
2024-11-23 14:15:31 -0500

    feat: implementar la seccion de equipo

--- js/main.js
+++ js/main.js
@@ -147,3 +147,31 @@ document.addEventListener("DOMContentLoaded", () => {
     startAutoScroll();
 });
 
+
+// Animación en scroll (reutilizable)
+document.addEventListener("DOMContentLoaded", () => {
+    const scrollElements = document.querySelectorAll(".animate-on-scroll");
+
+    const elementInView = (el, offset = 150) => {
+        const elementTop = el.getBoundingClientRect().top;
+        return (
+            elementTop <=
+            (window.innerHeight || document.documentElement.clientHeight) - offset
+        );
+    };
+
+    const displayScrollElement = (el) => {
+        el.classList.add("visible");
+    };
+
+    const handleScrollAnimation = () => {
+        scrollElements.forEach((el) => {
+            if (elementInView(el, 150)) {
+                displayScrollElement(el);
+            }
+        });
+    };
+
+    window.addEventListener("scroll", handleScrollAnimation);
+});
+
